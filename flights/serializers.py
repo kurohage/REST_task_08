0b47d7edb398594519935e94b5e3b0ed0ae05a21
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-23 16:51:56 +0300

    task completed. I officially hate serializers.

--- flights/serializers.py
+++ flights/serializers.py
@@ -10,17 +10,35 @@ class FlightSerializer(serializers.ModelSerializer):
 		model = Flight
 		fields = ['destination', 'time', 'price', 'id']
 
-
+# modify this to return the destination instead of the id, and return the flight, date and id
 class BookingSerializer(serializers.ModelSerializer):
+	flight = serializers.SlugRelatedField(
+			read_only=True,
+			slug_field='destination'
+		)
+
 	class Meta:
 		model = Booking
 		fields = ['flight', 'date', 'id']
 
-
+# Modify this to return more info
 class BookingDetailsSerializer(serializers.ModelSerializer):
+	total = serializers.SerializerMethodField() # total cost of flight for all passengers
+	#flight = serializers.SerializerMethodField() # detailed flight info
+	flight = FlightSerializer() # when using the Flight serializer, it'll auto feed the flight id since it's a foreign key in the Model relationship -- no need to create a function override
+
 	class Meta:
 		model = Booking
-		fields = ['flight', 'date', 'passengers', 'id']
+		#fields = ['flight', 'date', 'passengers', 'id'] # original fields
+		fields = ['total', 'flight', 'date', 'id', 'passengers']
+
+	def get_total(self, object):
+		return object.flight.price * object.passengers
+
+	# function override is not required since we already have the fligh tid which is a foreign key to the Flight model
+	#def get_flight(self, object):
+	#	info = Flight.objects.filter(bookings=object.id).first()
+	#	return FlightSerializer(instance=info, read_only=True).data
 
 
 class AdminUpdateBookingSerializer(serializers.ModelSerializer):
@@ -61,24 +79,32 @@ class SlugRelatedBookingField(serializers.SlugRelatedField):
 		return queryset
 """
 
+class UserInfoSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User	
+		fields = ['first_name', 'last_name']
+
 class ProfileSerializer(serializers.ModelSerializer):
 
-	first_name = serializers.SerializerMethodField()
-	last_name = serializers.SerializerMethodField()
-	#past_bookings = BookingSerializer(queryset=Booking.objects.filter(user=value.user,date__lt=date.today()))
+	#first_name = serializers.SerializerMethodField()
+	#last_name = serializers.SerializerMethodField()
+	user = serializers.SerializerMethodField()
 	past_bookings = serializers.SerializerMethodField()
 	tier = serializers.SerializerMethodField()
 	
 	class Meta:
 		model = Profile
 		#fields = ['user', 'miles', 'first_name', 'last_name'] # original order
-		fields = ['first_name', 'last_name', 'miles', 'past_bookings', 'tier']
+		fields = ['user', 'miles', 'past_bookings', 'tier']
 
-	def get_first_name(self, object):
-		return object.user.first_name
+	#def get_first_name(self, object):
+	#	return object.user.first_name
 
-	def get_last_name(self, object):
-		return object.user.last_name
+	#def get_last_name(self, object):
+	#	return object.user.last_name
+
+	def get_user(self, object):
+		return UserInfoSerializer(instance=object.user, read_only=True).data
 
 	# return bookings that have already passed (old)
 	def get_past_bookings(self, object):
